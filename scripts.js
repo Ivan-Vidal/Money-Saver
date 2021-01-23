762c5a0ccf14df24f876b3990c6bde029bb0a6e8
Ivan Vidal <ivanvalmeida.123@gmail.com>
2021-01-23 19:25:13 -0300

    config modal

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const Modal = {
+    open() {
+        //abrir modal
+        //adicionar a class active ao modal
+      document.querySelector('.modal-overlay').classList.add('active')
+    },
+    close() {
+        //fechar o modal
+        //remover a class active do modal
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
